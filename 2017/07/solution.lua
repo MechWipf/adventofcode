2e5ddc4e1bf0b1229a743519b308453e6d512890
Nils W <MechWipf@googlemail.com>
2018-03-07 15:33:37 +0100

    [changed] 07

--- 2017/07/solution.lua
+++ 2017/07/solution.lua
@@ -1,45 +1,59 @@
-package.path = [[X:\Notizen\OtherStuff\?.lua;]] .. package.path
+package.path = [[D:\Workspace\Projects\adventofcode\libs\?.lua;]] .. package.path
 local utils = require "utils"
 
 local function getTowers ( raw )
-    local n = raw:find "->"
-    return
-        n and raw:sub( n + 3, 1e9 ):gsub(" ",""):explode "," or nil,
-        raw:match( [[^([a-z]*).%(([0-9]*)%)]] )
+  local n = raw:find "->"
+  return
+    n and raw:sub( n + 3, 1e9 ):gsub(" ",""):explode "," or nil,
+    raw:match( [[^([a-z]*).%(([0-9]*)%)]] )
+end
+
+local function getWeight( tower, tree, childs, weight )
+  local w = weight[tower]
+
+  if childs[tower] then
+    for _, t in pairs(childs[tower]) do
+      w = w + getWeight( t, tree, childs, weight )
+    end
+  end
+
+  return w
 end
 
 local function main ( lines )
-    local tree = {}
-    local weight = {}
+  local tree = {}
+  local childs = {}
+  local weight = {}
 
-    for line in lines do
-        local subTowers, baseTower, baseTowerWeight = getTowers( line )
-        weight[baseTower] = baseTowerWeight
+  for line in lines do
+    local subTowers, baseTower, baseTowerWeight = getTowers( line )
+    weight[baseTower] = baseTowerWeight
 
-        if subTowers then
-            for k, tower in pairs( subTowers ) do
-                if tree[tower] then error "already exists" end
-                tree[tower] = baseTower
-            end
-        end
+    if subTowers then
+      childs[baseTower] = subTowers
+      for k, tower in pairs( subTowers ) do
+        if tree[tower] then error "already exists" end
+        tree[tower] = baseTower
+      end
     end
+  end
 
-    local last = nil
-    local m = next(tree)
-    while m do
-        last = m
-        m = tree[m]
+  local last = nil
+  local m = next(tree)
+  while m do
+    last = m
+    m = tree[m]
+  end
+
+  print(last)
+
+  local printed = {}
+  for k, tower in pairs( tree ) do
+    if tree[tower] == last and not printed[tower] then
+      printed[tower] = true
+      print(tower, getWeight(tower, tree, childs, weight))
     end
-
-    print(last)
-
-    local printed = {}
-    for k, tower in pairs( tree ) do
-        if tree[tower] == last and not printed[tower] then
-            printed[tower] = true
-            print(tower, weight[tower])
-        end
-    end
+  end
 end
 
 main( io.lines( select(1, ...) ) )
